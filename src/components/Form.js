8db50de3687f474eb11a0b73ca4bda4d18d3cf78
Emery Tate <tateejt@gmail.com>
2020-03-04 05:57:01 +0000

    Automatically backed up by Learn

--- src/components/Form.js
+++ src/components/Form.js
@@ -17,6 +17,12 @@ class Form extends React.Component {
       lastName: event.target.value
     })
   }
+  
+  handleSubmit = event => {
+    event.preventDefault()
+    let formData = { firstName: this.state.firstName, lastName: this.state.lastName }
+    this.sendFormDataSomewhere(formData)
+  }
 
   render() {
     return (

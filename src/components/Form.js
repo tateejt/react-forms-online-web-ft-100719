f3891352eb2033ed6d974721d5d72f52f51f64e4
Emery Tate <tateejt@gmail.com>
2020-03-04 05:50:25 +0000

    Automatically backed up by Learn

--- src/components/Form.js
+++ src/components/Form.js
@@ -20,9 +20,17 @@ class Form extends React.Component {
 
   render() {
     return (
-      <form>
-        <input type="text" name="firstName" onChange={event => this.handleFirstNameChange(event)} value={this.state.firstName} />
-        <input type="text" name="lastName" onChange={event => this.handleLastNameChange(event)} value={this.state.lastName} />
+      <form onSubmit={event => this.handleSubmit(event)}>
+        <input
+          type="text"
+          onChange={event => this.handleFirstNameChange(event)}
+          value={this.state.firstName}
+        />
+        <input
+          type="text"
+          onChange={event => this.handleLastNameChange(event)}
+          value={this.state.lastName}
+        />
       </form>
     )
   }
